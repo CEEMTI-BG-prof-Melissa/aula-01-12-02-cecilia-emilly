6eeabbcd95b9e7ee8839f4f95223a1523fcb8497
Emillycristine2008 <emilly.cbsousa@aluno.edu.es.gov.br>
2025-09-09 11:56:22 -0300

    Create index.php

---
+++ Aula 03- 3 Trimestre/index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Aula> 02 - PHP </title>
+    </head>
+
+    <body>
+        <?php
+        $nome1="Emilly";
+        $nome2="Cecilia";
+        $nome3="Duda Soares";
+
+        $n1=10;
+        $n2=2;
+
+        $soma=$n1+$n2;
+
+        echo "Meu nome é" .&nome1 ." meu trio é" &nome2 . $nome3;
+        echo "<br>Meu nome é $nome1 e minha dupla é $nome2";
+
+        echo "<br><br>A soma de $n1 com $n2 é igual a $soma.";
+         ?>
+    </body>
+</html> 
